7749e681e55d6cc83031832e7feae2244e9cfcb0
HMIMID <ahmedhmimid177@gmail.com>
2025-03-20 21:24:43 +0000

    feat: create a thread

--- models.js
+++ models.js
@@ -1,5 +1,10 @@
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+  () => {console.log('successfully connected')}
+).catch(errr => console.log(errr));
 
 const date = new Date();
 
