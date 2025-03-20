82038ce8f7febe0f2735a7f9fcdd2136b602ff10
HMIMID <ahmedhmimid177@gmail.com>
2025-03-20 21:18:47 +0000

    feat: create a thread

--- routes/api.js
+++ routes/api.js
@@ -1,66 +1,37 @@
-'use strict'
-require('dotenv').config();
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+"use strict";
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(
-  () => {console.log('successfully connected')}
-).catch(errr => console.log(errr));
-
-const dt = new Date();
-
-const ReplySchema = new Schema({
-  text: String,
-  delete_password: String,
-  reported: {type:Boolean, default: false},
-  created_on: {type:Boolean, default: dt},
-  bumped_on: {type:Boolean, default: dt},
-});
-
-const Reply = mongoose.model("Reply", ReplySchema);
-
-const ThreadSchema = new Schema({
-  text: String,
-  created_on: {type: Date, default: dt},
-  bumped_on: {type: Date, default: dt},
-  reported: {type:Boolean, default: false},
-  delete_password: String,
-  replies: [ReplySchema]
-});
-
-const Thread = mongoose.model("Thread", ThreadSchema);
-
-const BoardSchema = new Schema({
-  name: String,
-  threads: [ThreadSchema]
-});
-
-const Board = mongoose.model("Board", BoardSchema);
- 
+const BoardModel = require("../models").Board;
+const ThreadModel = require("../models").Thread;
+const ReplyModel = require("../models").Reply;
 
 module.exports = function (app) {
-
-  app.route('/api/threads/:board').post(
-    (req, res) => {
+  app
+    .route("/api/threads/:board")
+    .post((req, res) => {
       const { text, delete_password } = req.body;
       let board = req.body.board;
       if (!board) {
         board = req.params.board;
       }
-      const newThread = new Thread({
+      console.log("post", req.body);
+      const newThread = new ThreadModel({
         text: text,
         delete_password: delete_password,
         replies: [],
       });
-      Board.findOne({ name: board }, (err, Boarddata) => {
+      console.log("newThread", newThread);
+      BoardModel.findOne({ name: board }, (err, Boarddata) => {
         if (!Boarddata) {
-          const newBoard = new Board({
+          const newBoard = new BoardModel({
             name: board,
             threads: [],
           });
+          console.log("newBoard", newBoard);
           newBoard.threads.push(newThread);
           newBoard.save((err, data) => {
+            console.log("newBoardData", data);
             if (err || !data) {
+              console.log(err);
               res.send("There was an error saving in post");
             } else {
               res.json(newThread);
@@ -77,9 +48,164 @@ module.exports = function (app) {
           });
         }
       });
-    }
-  );
-    
-  // app.route('/api/replies/:board');
-};
+    })
+    .get((req, res) => {
+      const board = req.params.board;
+      BoardModel.findOne({ name: board }, (err, data) => {
+        if (!data) {
+          console.log("No board with this name");
+          res.json({ error: "No board with this name" });
+        } else {
+          console.log("data", data);
+          const threads = data.threads.map((thread) => {
+            const {
+              _id,
+              text,
+              created_on,
+              bumped_on,
+              reported,
+              delete_password,
+              replies,
+            } = thread;
+            return {
+              _id,
+              text,
+              created_on,
+              bumped_on,
+              reported,
+              delete_password,
+              replies,
+              replycount: thread.replies.length,
+            };
+          });
+          res.json(threads);
+        }
+      });
+    })
+    .put((req, res) => {
+      console.log("put", req.body);
+      const { report_id } = req.body;
+      const board = req.params.board;
+      BoardModel.findOne({ name: board }, (err, boardData) => {
+        if (!boardData) {
+          res.json("error", "Board not found");
+        } else {
+          const date = new Date();
+          let reportedThread = boardData.threads.id(report_id);
+          reportedThread.reported = true;
+          reportedThread.bumped_on = date;
+          boardData.save((err, updatedData) => {
+            res.send("Success");
+          });
+        }
+      });
+    })
+    .delete((req, res) => {
+      console.log("delete", req.body);
+      const { thread_id, delete_password } = req.body;
+      const board = req.params.board;
+      BoardModel.findOne({ name: board }, (err, boardData) => {
+        if (!boardData) {
+          res.json("error", "Board not found");
+        } else {
+          let threadToDelete = boardData.threads.id(thread_id);
+          if (threadToDelete.delete_password === delete_password) {
+            threadToDelete.remove();
+          } else {
+            res.send("Incorrect Password");
+            return;
+          }
+          boardData.save((err, updatedData) => {
+            res.send("Success");
+          });
+        }
+      });
+    });
 
+  app
+    .route("/api/replies/:board")
+    .post((req, res) => {
+      console.log("thread", req.body);
+      const { thread_id, text, delete_password } = req.body;
+      const board = req.params.board;
+      const newReply = new ReplyModel({
+        text: text,
+        delete_password: delete_password,
+      });
+      BoardModel.findOne({ name: board }, (err, boardData) => {
+        if (!boardData) {
+          res.json("error", "Board not found");
+        } else {
+          const date = new Date();
+          let threadToAddReply = boardData.threads.id(thread_id);
+          threadToAddReply.bumped_on = date;
+          threadToAddReply.replies.push(newReply);
+          boardData.save((err, updatedData) => {
+            res.json(updatedData);
+          });
+        }
+      });
+    })
+    .get((req, res) => {
+      const board = req.params.board;
+      BoardModel.findOne({ name: board }, (err, data) => {
+        if (!data) {
+          console.log("No board with this name");
+          res.json({ error: "No board with this name" });
+        } else {
+          console.log("data", data);
+          const thread = data.threads.id(req.query.thread_id);
+          res.json(thread);
+        }
+      });
+    })
+    .put((req, res) => {
+      //       thread_id: 60898569e083081d56e290cf
+      //       reply_id: 608986aee083081d56e290d0
+      const { thread_id, reply_id } = req.body;
+      const board = req.params.board;
+      BoardModel.findOne({ name: board }, (err, data) => {
+        if (!data) {
+          console.log("No board with this name");
+          res.json({ error: "No board with this name" });
+        } else {
+          console.log("data", data);
+          let thread = data.threads.id(thread_id);
+          let reply = thread.replies.id(reply_id);
+          reply.reported = true;
+          reply.bumped_on = new Date();
+          data.save((err, updatedData) => {
+            if (!err) {
+              res.send("Success");
+            }
+          });
+        }
+      });
+    })
+    .delete((req, res) => {
+      const { thread_id, reply_id, delete_password } = req.body;
+      console.log("delete reply body", req.body);
+      const board = req.params.board;
+      BoardModel.findOne({ name: board }, (err, data) => {
+        if (!data) {
+          console.log("No board with this name");
+          res.json({ error: "No board with this name" });
+        } else {
+          console.log("data", data);
+          let thread = data.threads.id(thread_id);
+          let reply = thread.replies.id(reply_id);
+          if (reply.delete_password === delete_password) {
+            reply.remove();
+          } else {
+            res.send("Incorrect Password");
+            return;
+          }
+          data.save((err, updatedData) => {
+            if (!err) {
+              res.send("Success");
+            }
+          });
+        }
+      });
+    });
+};
